85b924fc2962745ab32cff2395b9713bcb6e8cac
Ubuntu <stevenm@steven-vm.u0mdbf3sdj1uxj5shit42bfglb.bx.internal.cloudapp.net>
2021-11-03 15:21:47 +0000

    Finds first 10 movies of hardcoded value: Jean Reno

---
+++ get_imdb.py
@@ -0,0 +1,22 @@
+import requests
+import os
+import json
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+BASE_URL = "https://imdb-api.com/en/API"
+API_KEY = os.getenv("API_KEY")
+
+def get_name():
+    r = requests.get(BASE_URL + "/SearchName/" + API_KEY + "/Jean Reno")
+    r = r.json()
+    #print(json.dumps(r, indent=4))
+    #print(r["results"][0]["id"])
+    actor_id = r["results"][0]["id"]
+    search_name(actor_id)
+def search_name(actor_id):
+    r = requests.get(BASE_URL + "/Name/" + API_KEY + actor_id)
+    r = r.json()
+    print(r)
+get_name()
