f28986cf1c305340ec845e216687cbe24d9c9967
Devang Patel <devangminer722@gmail.com>
2021-11-04 12:45:57 -0400

    got login with google working

--- routes.py
+++ routes.py
@@ -49,13 +49,62 @@ def login():
 
     request_uri = client.prepare_request_uri(
         authorization_endpoint,
-        redirect_uri=request.base_url + "/callback",
-        scope=["openid", "email", "profile"],
+        redirect_uri=request.base_url + '/callback',
+        scope=['openid', 'email', 'profile'],
     )
 
-    print(request_uri)
+    return redirect(request_uri)
 
-    return redirect('/')
+@app.route('/api/login/callback')
+def login_callback():
+    # get code from google
+    code = request.args.get('code')
+
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg['token_endpoint']
+
+    # Prepare and send a request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Now that you have tokens (yay) let's find and hit the URL
+    # from Google that gives you the user's profile information,
+    # including their Google profile image and email
+    userinfo_endpoint = google_provider_cfg['userinfo_endpoint']
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    # You want to make sure their email is verified.
+    # The user authenticated with Google, authorized your
+    # app, and now you've verified their email through Google!
+    if userinfo_response.json().get("email_verified"):
+        user_id = userinfo_response.json()["sub"]
+        email = userinfo_response.json()["email"]
+        username = userinfo_response.json()["given_name"]
+
+        print(user_id)
+        print(email)
+        print(username)
+        
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    return 'hi'
 
     
 
@@ -70,5 +119,6 @@ def home():
 
 if __name__ == "__main__":
     app.run(
-        debug=True
+        debug=True,
+        ssl_context='adhoc'
     )
