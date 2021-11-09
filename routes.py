382615670b467e4d96cb7a36f1dfc74457428172
Devang Patel <devangminer722@gmail.com>
2021-11-09 10:02:36 -0500

    removed unnecessary prints

--- routes.py
+++ routes.py
@@ -97,8 +97,7 @@ def login_callback():
                 'auth_token': auth_token, 
                 'username': user.username
             }
-            print(output)
-            print(decode_auth_token(output['auth_token']))
+            
             return make_response(jsonify(output)), 200
 
         
