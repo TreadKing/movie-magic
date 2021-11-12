52562b0029f6895a9daf3d07151b9f42af16e2a1
Nana Krampah <ndarko1@student.gsu.edu>
2021-11-12 03:06:30 +0000

    Return results from API call

--- routes.py
+++ routes.py
@@ -15,6 +15,7 @@ from flask.json import jsonify
 from oauthlib.oauth2 import WebApplicationClient
 
 from app import app
+
 # from models import User
 from auth_token import encode_auth_token, decode_auth_token
 from firebase_admin import db
@@ -22,51 +23,50 @@ from firebase_admin import db
 from get_movie import search_movie_by_actor, search_movie_by_text
 
 
-
 # Configuration
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
-GOOGLE_DISCOVERY_URL = (
-    "https://accounts.google.com/.well-known/openid-configuration"
-)
+GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
 
 # OAuth 2 client setup
 client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
 
 def get_google_provider_cfg():
     return requests.get(GOOGLE_DISCOVERY_URL).json()
 
 
-@app.route('/api/login', methods=['POST'])
+@app.route("/api/login", methods=["POST"])
 def login():
 
     google_provider_cfg = get_google_provider_cfg()
-    authorization_endpoint = google_provider_cfg['authorization_endpoint']
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
     request_uri = client.prepare_request_uri(
         authorization_endpoint,
-        redirect_uri=request.base_url + '/callback',
-        scope=['openid', 'email', 'profile'],
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
     )
 
     return redirect(request_uri)
 
-@app.route('/api/login/callback')
+
+@app.route("/api/login/callback")
 def login_callback():
     # get code from google
-    code = request.args.get('code')
+    code = request.args.get("code")
 
     # Find out what URL to hit to get tokens that allow you to ask for
     # things on behalf of a user
     google_provider_cfg = get_google_provider_cfg()
-    token_endpoint = google_provider_cfg['token_endpoint']
+    token_endpoint = google_provider_cfg["token_endpoint"]
 
     # Prepare and send a request to get tokens! Yay tokens!
     token_url, headers, body = client.prepare_token_request(
         token_endpoint,
         authorization_response=request.url,
         redirect_url=request.base_url,
-        code=code
+        code=code,
     )
     token_response = requests.post(
         token_url,
@@ -79,7 +79,7 @@ def login_callback():
     client.parse_request_body_response(json.dumps(token_response.json()))
 
     #  get user info from google login
-    userinfo_endpoint = google_provider_cfg['userinfo_endpoint']
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
     uri, headers, body = client.add_token(userinfo_endpoint)
     userinfo_response = requests.get(uri, headers=headers, data=body)
 
@@ -91,31 +91,26 @@ def login_callback():
         auth_token = encode_auth_token(str(user_id))
 
         if auth_token:
-            output = {
-                'auth_token': auth_token, 
-                'username': username
-            }
+            output = {"auth_token": auth_token, "username": username}
 
-            users_ref = db.reference('/').child('Users').child(str(user_id))
+            users_ref = db.reference("/").child("Users").child(str(user_id))
             if not users_ref.get():
-                print(f'new user: {user_id}')
-                users_ref.set({
-                    "Name": username
-                })
-            
+                print(f"new user: {user_id}")
+                users_ref.set({"Name": username})
+
             else:
-                print(f'user {user_id} already exists')
-            
-            
+                print(f"user {user_id} already exists")
+
             return make_response(jsonify(output), 200)
 
-        
     else:
         return make_response("User email not available or not verified by Google.", 400)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('login.html')
+    return render_template("login.html")
+
 
 @app.route("/search/actor", methods=["POST"])
 def save_actor():
@@ -127,7 +122,6 @@ def save_actor():
         return flask.redirect(flask.url_for("search"))
 
     # Output results from api_results
-    
 
 
 @app.route("/search/movie", methods=["POST"])
@@ -139,21 +133,27 @@ def save_movie():
         flask.flash("Enter a valid movie name")
         return flask.redirect(flask.url_for("search"))
     # Output results from movie_text
+    return movie_text
 
 
 @app.route("/getWatchlist", methods=["POST"])
 def getList():
     """Gets information from db to output to the user their watchlist"""
-    #Query information from db pertaining to user
+    # Query information from db pertaining to user
     print(request.args)
-    auth_token = request.args['auth_token']
+    auth_token = request.args["auth_token"]
 
     user_id = decode_auth_token(auth_token)
 
-    if user_id == 'Invalid token. Please log in again.':
-        return make_response(jsonify({'error': 'Invalid token. Please log in again.'})), 500
+    if user_id == "Invalid token. Please log in again.":
+        return (
+            make_response(jsonify({"error": "Invalid token. Please log in again."})),
+            500,
+        )
 
-    watch_list_ref = db.reference('/').child('Users').child(str(user_id)).child('WatchList')
+    watch_list_ref = (
+        db.reference("/").child("Users").child(str(user_id)).child("WatchList")
+    )
     watch_list = watch_list_ref.get()
 
     return make_response(jsonify(watch_list), 200)
@@ -162,25 +162,30 @@ def getList():
 @app.route("/addToWatchlist", methods=["POST"])
 def addToList():
     """After adding to the watchlist, send the user to the watchlist to see their change"""
-    '''
+    """
     auth_token
     staus (unwatched, watching, dropped, finished)
     movieID
-    '''
+    """
 
-    auth_token = request.args['auth_token']
+    auth_token = request.args["auth_token"]
     user_id = decode_auth_token(auth_token)
-    if user_id == 'Invalid token. Please log in again.':
-        return make_response(jsonify({'error': 'Invalid token. Please log in again.'}), 500)
+    if user_id == "Invalid token. Please log in again.":
+        return make_response(
+            jsonify({"error": "Invalid token. Please log in again."}), 500
+        )
 
-    status = request.args['status']
-    movie_id = request.args['MovieID']
+    status = request.args["status"]
+    movie_id = request.args["MovieID"]
 
-    movie_id_ref = db.reference('/').child('Users').child(str(user_id)).child('WatchList').child(str(movie_id))
-    movie_id_ref.set({
-        'Status': status
-    })
-
+    movie_id_ref = (
+        db.reference("/")
+        .child("Users")
+        .child(str(user_id))
+        .child("WatchList")
+        .child(str(movie_id))
+    )
+    movie_id_ref.set({"Status": status})
 
     # Send user to view their own watchlist
     return make_response(200)
@@ -189,9 +194,9 @@ def addToList():
 @app.route("/deleteFromWatchlist", methods=["POST"])
 def deleteFromList(movie_id):
     """Find a movie object in the db and delete that entry from the watchlist"""
-    auth_token = request.args['auth_token']
+    auth_token = request.args["auth_token"]
     user_id = decode_auth_token(auth_token)
-    movie_id = request.args['movieID']
+    movie_id = request.args["movieID"]
 
     ref = db.reference("Users").child(user_id).child("WatchList")
     watchlist = ref.get()
@@ -212,7 +217,7 @@ def addFriend(friend_id):
     """Given a friend id, add an id to a user's friendlist"""
     # for sprint 2
     ...
-    
+
 
 @app.route("/deleteFromFriendsList", methods=["POST"])
 def deleteFriend(friend_id):
@@ -233,8 +238,6 @@ def getUsers():
     # don't think we need till sprint 2
     ...
 
+
 if __name__ == "__main__":
-    app.run(
-        debug=True,
-        ssl_context='adhoc'
-    )
+    app.run(debug=True, ssl_context="adhoc")
