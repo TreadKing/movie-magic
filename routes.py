73a118a1ba937fcd0e991c586cfe367d04dd458b
Devang Patel <devangminer722@gmail.com>
2021-11-26 18:40:20 -0500

    PORT FIX

--- routes.py
+++ routes.py
@@ -387,7 +387,7 @@ def getusers():
 app.register_blueprint(bp)
 
 if __name__ == "__main__":
-    if os.getenv("port"):
+    if os.getenv("PORT"):
         app.run(
             host=os.getenv("IP", "0.0.0.0"),
             port=int(os.getenv("PORT", 8080)),
