314adce4ab801b57b03f20586c35adfa1ff4eb12
Devang Patel <devangminer722@gmail.com>
2021-11-26 18:12:40 -0500

    heroku fix

--- routes.py
+++ routes.py
@@ -387,12 +387,11 @@ def getusers():
 app.register_blueprint(bp)
 
 if __name__ == "__main__":
-    if os.getenv("port"):
+    if os.getenv("PORT"):
         app.run(
             host=os.getenv("IP", "0.0.0.0"),
             port=int(os.getenv("PORT", 8080)),
             debug=True,
-            ssl_context="adhoc",
         )
     else:
         app.run(debug=True)
