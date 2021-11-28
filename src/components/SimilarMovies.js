1ebf4937f5c9f887ce035db1754bd5146dfdbff5
glimmerray <bhodges7@student.gsu.edu>
2021-11-28 16:42:30 -0500

    frontend first attempt

---
+++ src/components/SimilarMovies.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import MovieList from './MovieList.js';
+
+function SimilarMovies(props) {
+    const listOfMovies = props.listOfMovies
+    const title = props.title
+
+    return <>
+        <span
+            className="similar-movies-title"
+        >
+            Similar movies for: {title}
+        </span>
+        <MovieList listOfMovies={listOfMovies} />
+    </>
+
+
+}
+
+export default SimilarMovies;
