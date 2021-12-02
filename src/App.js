eb42344213ebdff50189b78e9e43e391dffdc14b
Devang Patel <devangminer722@gmail.com>
2021-12-02 18:15:15 -0500

    started linting js files - landing, api, app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
+import React from 'react';
 import Login from './components/Login';
 // import MovieSearch from './components/MovieSearch.js';
-
 
 function App() {
   // var authToken = document.cookie.slice(11)
